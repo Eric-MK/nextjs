d407a70431b1373c779bf06fa37217d784f4b33e
JoakBouy <b.joak@alustudent.com>
2024-06-14 09:21:32 +0300

    Added erp

--- components/Offer.jsx
+++ components/Offer.jsx
@@ -187,6 +187,39 @@ const Offer = () => {
             className={styles.card}
           >
             <motion.h1 variants={textVariants} className={styles.card__num}>
+             
+            </motion.h1>
+            <motion.div
+              initial="offscreen"
+              whileInView="onscreen"
+              transition={{ staggerChildren: 0.5 }}
+              className={styles.card__text}
+            >
+              <motion.h3 variants={textVariants}>EPR (Emergency Response Program)</motion.h3>
+              <motion.p variants={textVariants}>
+                This is the cleaning and sanitization of offices to curb the spread of COVID-19 and other diseases
+              </motion.p>
+            </motion.div>
+            <div className={styles.card__img}>
+              <img src="/images/cleaning.jpg" alt="cleaning" />
+              <img
+                src="/svgs/ic-arrow-right.svg"
+                alt="arrow-right"
+                width={75}
+                height={75}
+                className={styles.arrow__right}
+              />
+            </div>
+          </motion.div>
+
+          <motion.div
+            initial="offscreen"
+            whileInView="onscreen"
+            transition={{ staggerChildren: 0.5 }}
+            variants={cardVariants}
+            className={styles.card}
+          >
+            <motion.h1 variants={textVariants} className={styles.card__num}>
               
             </motion.h1>
             <motion.div
